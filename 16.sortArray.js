70abb23b56eb082c0da726426f48be10a4301d3f
dusengepeggy <135857264+dusengepeggy@users.noreply.github.com>
2023-11-14 11:56:02 +0200

    question 16,17,18

--- 16.sortArray.js
+++ 16.sortArray.js
@@ -1,10 +1,13 @@
 function sortArray(arr){
-    var sortedarr= arr.sort((a,b)=>a-b);
-    for (i=0;i<arr.length;i++){
-        if(i<3||i>=n-3){
+    console.log("first 3 element before sorting",arr.slice(0,3));
+    console.log("last 3 element before sorting",arr.slice(-3));
 
-        }
-
-    }
+    var sortedArr= arr.sort((a,b)=>a-b);
+    
+    var first3=sortedArr.slice(0,3) ;
+    console.log( "sorted array:",sortedArr);
+    console.log("first 3 elements of the sorted array:",first3);
+    
 
 }
+sortArray([12,34,5,6,9,59,3,2]);
